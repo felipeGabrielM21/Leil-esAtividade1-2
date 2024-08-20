afc7029a0a72700d870db86a403a15039ee02a08
Felipe Gabriel <felipe.gabrielmar2006@gmail.com>
2024-08-20 10:28:30 -0300

    Adicionando lista de produtos

---
+++ LeiloesTDSat.rar/README.md
@@ -0,0 +1,10 @@
+
+Nome do Projeto: Leilões
+
+Explicação sobre o Projeto:
+Este projeto foi desenvolvido para organizar e gerenciar um sistema de leilões. Ele permite a realização de operações básicas de CRUD (Criar, Ler, Atualizar e Deletar) para gerenciar itens em leilão.
+
+Tecnologias Utilizadas:
+
+Java: Utilizado para o desenvolvimento do backend do sistema, incluindo a lógica de negócios e a gestão dos dados.
+MySQL: Utilizado como banco de dados para armazenar as informações dos itens em leilão, usuários, lances, entre outros dados essenciais para o funcionamento do sistema.
---
+++ src/ProdutosDAO.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adm
+ */
+import static com.mysql.cj.conf.PropertyKey.PASSWORD;
+import static com.mysql.cj.conf.PropertyKey.USER;
+import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ProdutosDAO {
+
+    private static final String URL = "jdbc:mysql://localhost/Leiloes";
+    private static final String USER = "root";
+    private static final String PASSWORD = "fefe21@";
+
+    Connection conn;
+    PreparedStatement prep;
+    ResultSet resultset;
+
+    public void cadastrarProduto(ProdutosDTO produto) throws SQLException {
+
+        String sql = "INSERT INTO cadastros(nome, valor, status) VALUES (?, ?, ?)";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD); PreparedStatement pt = conn.prepareStatement(sql)) {
+
+            pt.setString(1, produto.getNome());
+            pt.setInt(2, produto.getValor());
+            pt.setString(3, produto.getStatus());
+            pt.executeUpdate();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + ex.getMessage());
+        }
+    }
+
+    public ArrayList<ProdutosDTO> listarProdutos() throws SQLException {
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+
+        String sql = "SELECT * FROM cadastros";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD); PreparedStatement pstmt = conn.prepareStatement(sql); ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                ProdutosDTO produtos = new ProdutosDTO();
+
+                produtos.setId(rs.getInt("id"));
+                produtos.setNome(rs.getString("nome"));
+                produtos.setValor(rs.getInt("valor"));
+                produtos.setStatus(rs.getString("status"));
+
+                listagem.add(produtos);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + ex.getMessage());
+        }
+
+        return listagem;
+    }
+}
+
